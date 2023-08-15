ce25fde9e2b213b913751258a88b7524a170a02c
François Onimus <francois.onimus@gmail.com>
2023-08-15 13:54:26 +0200

    add exiftool, add video extensions for iina

--- macos.sh
+++ macos.sh
@@ -170,9 +170,13 @@ defaults write com.apple.loginwindow AdminHostInfo HostName
 mv $HOME/Library/Preferences/com.apple.dock.plist $HOME/Library/Preferences/com.apple.dock.plist.BACKUP
 cp com.apple.dock.plist $HOME/Library/Preferences/com.apple.dock.plist
 
-EXTENSIONS=(txt yml xml sh properties json js java ts kt css env conf)
-for extension in "${EXTENSIONS[@]}"; do
+SUBLIME_EXTENSIONS=(txt yml xml sh properties json js java ts kt css env conf)
+for extension in "${SUBLIME_EXTENSIONS[@]}"; do
     duti -s com.sublimetext.4 $extension all
+done
+IINA_EXTENSIONS=(mp4 avi mkv)
+for extension in "${IINA_EXTENSIONS[@]}"; do
+    duti -s com.colliderli.iina $extension all
 done
 
 for app in "Dock" "Finder"; do
